53e49e128ea3d8bcf2ea38face334ca8b5d005b4
htejadag <htejadag25@gmail.com>
2023-10-04 13:10:29 -0500

    MsTransaction - Implementar método get findAll
    Probar conexión a bd

--- Transaction/src/main/java/POLYGLOT/MS/TEST/Transaction/TransactionApplication.java
+++ Transaction/src/main/java/POLYGLOT/MS/TEST/Transaction/TransactionApplication.java
@@ -2,8 +2,10 @@ package POLYGLOT.MS.TEST.Transaction;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class TransactionApplication {
 
 	public static void main(String[] args) {
